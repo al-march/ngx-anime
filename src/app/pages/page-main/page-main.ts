35d914c8f5ba5beca5ed4f03f2a0dc5bea8bc4c0
Alexander <valverin95@gmail.com>
2026-03-30 09:55:37 +0300

    feat: simple example

--- src/app/pages/page-main/page-main.ts
+++ src/app/pages/page-main/page-main.ts
@@ -1,11 +1,14 @@
 import { Navbar } from '@/app/shared/template/navbar/navbar';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { AnimeEnter, AnimeLeave } from 'ngx-animejs';
 
 @Component({
   selector: 'app-page-main',
-  imports: [Navbar],
+  imports: [Navbar, AnimeEnter, AnimeLeave],
   templateUrl: './page-main.html',
   styleUrl: './page-main.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageMain { }
+export class PageMain {
+  show = signal(false);
+}
