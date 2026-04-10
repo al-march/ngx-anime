b14d8ad8436754640bb58eede9d217e95ecf863d
daraivanova <kudiyarova.1975@gmail.com>
2026-04-11 00:08:19 +0300

    feat: add sidebar-state

---
+++ src/app/shared/template/sidebar/sidebar-state.ts
@@ -0,0 +1,22 @@
+import { effect, inject, InjectionToken, PLATFORM_ID, signal, type WritableSignal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { WA_LOCAL_STORAGE } from '@ng-web-apis/common';
+
+export const SIDEBAR_STATE = new InjectionToken<WritableSignal<boolean>>('SIDEBAR_STATE', {
+  factory: () => {
+    const platformId = inject(PLATFORM_ID);
+    const isBrowser = isPlatformBrowser(platformId);
+    const storage = isBrowser ? inject(WA_LOCAL_STORAGE) : null;
+    const saved = storage?.getItem('SIDEBAR');
+    const expand = signal(saved ? JSON.parse(saved) : true);
+
+    // TODO: сохранять в lS
+    effect(() => {
+      const value = String(expand());
+
+      storage?.setItem('SIDEBAR', value);
+    });
+
+    return expand;
+  },
+});
